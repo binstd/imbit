6e5b8a741607a0adc4358d1478e58e9262e2716b
ethluz <lvxing67@126.com>
2019-02-14 12:29:24 +0800

    恢复android配置

---
+++ android-old/app/src/main/java/com/imbit/MainActivity.java
@@ -0,0 +1,22 @@
+package com.imbit;
+
+// import com.facebook.react.ReactActivity;
+// new reactnativenavigation start
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+
+}
+// new reactnativenavigation end
+
+// public class MainActivity extends ReactActivity {
+
+//     /**
+//      * Returns the name of the main component registered from JavaScript.
+//      * This is used to schedule rendering of the component.
+//      */
+//     @Override
+//     protected String getMainComponentName() {
+//         return "imbit";
+//     }
+// }
